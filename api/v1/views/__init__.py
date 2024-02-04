73cd2654625709b36e520b464e1119510bffc0d0
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-05 00:55:15 +0100

    autopep8 revert
    Signed-off-by: Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Blueprint API."""
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__)
 '''blueprint for the AirBnB clone API'''
+
+from api.v1.views.index import *
+from api.v1.views.states import *
