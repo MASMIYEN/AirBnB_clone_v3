32fb0060ed8e1920ee85b4aa51ddf2ee10569efd
ahmedyak1 <ahmedyak1@github.com>
2024-02-04 01:28:36 +0000

    fixing issue in task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,36 @@
-#!/usr/bin/python3xx
-'''Status API'''
-import models
-from models import storage
-from models.base_model import BaseModel
+#!/usr/bin/python3
+'''Index  API '''
 from flask import jsonify
+
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return '''
+@app_views.route('/status')
+def get_status():
+    '''status of API
+    '''
     return jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''Response of JSON'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route('/stats')
+def get_stats():
+    '''number of objjects for each type
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
