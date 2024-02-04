e0db1124c4c9a5cab1b7aae41ae7106aaedb43e5
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-02-04 22:54:20 +0100

    rename methods and code rewrite
    Signed-off-by: Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,24 +13,25 @@ from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    '''status of API
-    '''
-    return jsonify(status="OK")
+def status():
+    return {
+        "status": "OK",
+    }
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    '''number of objjects for each type
-    '''
-    objects = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
+def stats():
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
     }
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
